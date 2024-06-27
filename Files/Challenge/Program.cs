12e37ed2c30b054210be39ce8033dc67bf2554c9
sagefairy <christosebeatu92@gmail.com>
2024-06-27 18:30:35 +0200

    Initial commit.

---
+++ Files/Challenge/Program.cs
@@ -0,0 +1,68 @@
+﻿
+//Christos Ebeatu BU/22c/IT/7789
+
+const string dirname = "FileCollection";
+const string filename = "results.txt";
+
+bool IsOfficeFile(string filename)
+{
+    string fileExtension = Path.GetExtension(filename);
+
+    if (fileExtension == ".xlsx" || fileExtension == ".docx" || fileExtension == ".pptx")
+    {
+        return true;
+    }
+    return false;
+}
+
+int pptxCount = 0, xlsxCount = 0, docxCount = 0, totalCount = 0;
+long pptxSize = 0, xlsxSize = 0, docxSize = 0, totalSize = 0;
+
+DirectoryInfo dirInfo = new(dirname);
+
+List<string> directories = new(Directory.EnumerateFiles(dirname));
+foreach (string file in directories)
+{
+    Console.WriteLine(file);
+    if (IsOfficeFile(file))
+    {
+        FileInfo fileInfo = new(file);
+
+        if (fileInfo.Extension == ".xlsx")
+        {
+            xlsxCount++;
+            xlsxSize += fileInfo.Length;
+        }
+        if (fileInfo.Extension == ".pptx")
+        {
+            pptxCount++;
+            pptxSize += fileInfo.Length;
+        }
+        if (fileInfo.Extension == ".docx")
+        {
+            docxCount++;
+            docxSize += fileInfo.Length;
+        }
+    }
+}
+
+totalSize = pptxSize + xlsxSize + docxSize;
+totalCount = pptxCount + xlsxCount + docxCount;
+
+
+using StreamWriter sw = File.CreateText(filename);
+sw.WriteLine($"Word File Count: {docxCount}");
+sw.WriteLine($"Excel File Count: {xlsxCount}");
+sw.WriteLine($"Powerpoint File Count: {pptxCount}");
+sw.WriteLine($"Word File Size: {docxSize}");
+sw.WriteLine($"Excel File Size: {xlsxSize}");
+sw.WriteLine($"Powerpoint File Size: {pptxSize}");
+sw.WriteLine($"Total Count: {totalCount}");
+sw.WriteLine($"Total Size: {totalSize}");
+
+
+sw.Close();
+
+
+
+
---
+++ Regex/Challenge/Program.cs
@@ -0,0 +1,40 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+//Christos Ebeatu BU/22c/IT/7789
+
+using System.Diagnostics;
+using System.Text.RegularExpressions;
+
+
+string ReverseDateFormat(string datestr) {
+    TimeSpan Timeout = TimeSpan.FromSeconds(1);
+    
+    Stopwatch sw = Stopwatch.StartNew();
+    Regex DateMatch = new(@"(?<month>\d{1,2})/(?<day>\d{1,2})/(?<year>\d{2,4})", RegexOptions.None, Timeout); 
+    Match match = DateMatch.Match(datestr);
+    sw.Stop();
+    
+    string month = match.Result("${month}");
+    string day = match.Result("${day}");
+    string year = match.Result("${year}");
+    
+    return $"{year}-{day}-{month}";
+}
+
+while(true){
+    Console.WriteLine("Enter Date(mm/dd/yyyy)");
+    string date = Console.ReadLine();
+    
+    if(date == "exit"){
+        break;
+    }
+    
+    if(DateOnly.TryParse(date, out DateOnly _)) {
+        Console.WriteLine(ReverseDateFormat(date));
+        
+    }
+    else{
+        Console.WriteLine("Invalid date");
+    }
+    
+}
